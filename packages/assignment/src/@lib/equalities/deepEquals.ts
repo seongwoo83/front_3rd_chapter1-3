14456301089b6de8e223d9d262905defd0057fb0
jsw <jeongseongwoo83@daum.net>
2024-10-08 15:19:13 +0900

    FEAT : 비교함수 작성

--- packages/assignment/src/@lib/equalities/deepEquals.ts
+++ packages/assignment/src/@lib/equalities/deepEquals.ts
@@ -1,4 +1,29 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function deepEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+    if (objA === objB) return true;
+
+    if (Array.isArray(objA) && Array.isArray(objB)) {
+        if (objA.length !== objB.length) return false;
+
+        for (let i = 0; i < objA.length; i++) {
+            if (!deepEquals(objA[i], objB[i])) return false;
+        }
+        return true;
+    } else if (Array.isArray(objA) || Array.isArray(objB)) {
+        return false;
+    }
+
+    if (typeof objA !== 'object' || typeof objB !== 'object' || objA === null || objB === null) {
+        return false;
+    }
+
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+    if (keysA.length !== keysB.length) return false;
+
+    for (const key of keysA) {
+        if (!deepEquals(objA[key], objB[key])) return false;
+    }
+
+    return true;
 }
--- packages/assignment/src/@lib/equalities/shallowEquals.ts
+++ packages/assignment/src/@lib/equalities/shallowEquals.ts
@@ -1,4 +1,43 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function shallowEquals(objA: any, objB: any): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+  if (typeof objA !== 'object' || objA === null ||
+      typeof objB !== 'object' || objB === null) {
+    return false;
+  }
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+  for (const key of keysA) {
+    if (!Object.prototype.hasOwnProperty.call(objB, key) || objA[key] !== objB[key]) {
+      return false;
+    }
+  }
+  return true;
 }
+// export function shallowEquals(objA: any, objB: any): boolean {
+//   if(objA === objB){
+//     return true;
+//   }
+//   if(typeof objA !== 'object' || typeof objB !== 'object'){
+//     return false;
+//   }else{
+//     const keyOfA : string[] = Object.keys(objA);
+//     const keyOfB : string[] = Object.keys(objB);
+
+//     if(keyOfA.length !== keyOfB.length){
+//       return false;
+//     }
+//     for(const key of keyOfA){
+//       if(!keyOfB.includes(key) || objA[key] !== objB[key]){
+//         return false;
+//       }
+//     }
+//   }
+
+//   return true;
+// }
